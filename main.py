c6f51c2e2e73cc834b2089fcc2e601b7f3c904e2
brunsilva-jc <brunsilva.jc@gmail.com>
2024-09-06 09:22:45 -0300

    changes on main module

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
 import requests
 
+from people import People
 from planet import Planet
+import matplotlib.pyplot as plt
 
 import pandas as pd
 
 base_url = "https://swapi.dev/api/"
 
+people_endpoint = "people/"
+
 planets_endpoint = "planets/"
 
-def main():
-    planets_list = []
+planets_list = []
+people_list = []
 
+def main():
+    #get_planets()
+    get_people()
+
+def get_people():
+    response = requests.get(base_url + people_endpoint)
+    if response.status_code == 200:
+        people = response.json()
+        for p in people['results']:
+            name = p['name']
+            gender = p['gender']
+            height = p['height']
+            birth_year = p['birth_year']
+
+            peopleData = People(name, gender, height, birth_year)
+            people_list.append(peopleData)
+
+        data_people = create_dataframe(people_list)
+        print(data_people)
+        data_people['Height'].astype(float)
+        data_people.plot.scatter(x="Name", y="Height", s=200)
+        plt.show()
+    else:
+        print(f"Erro: {response.status_code}")
+
+def get_planets():
     response = requests.get(base_url + planets_endpoint)
 
     if response.status_code == 200:
@@ -33,16 +63,18 @@ def main():
 
 #showing dataframe for population on planets
 def create_dataframe(data):
-    planets_list = []
+    people_list_name = []
+    people_list_height = []
     for dt in data:
-        planet_dict = {
-            "Name": dt.name,
-            "Population": dt.population,
-            "Climate": dt.climate,
-        }
-        planets_list.append(planet_dict)
+        people_list_name.append(dt.name)
+        people_list_height.append(dt.height)
 
-    planets_dataframe = pd.DataFrame(planets_list)
-    return planets_dataframe
+    people_dict = {
+        "Name": people_list_name,
+        "Height": people_list_height
+    }
+
+    people_dataframe = pd.DataFrame(people_dict)
+    return people_dataframe
 
 main()
