2519c860f1becedffe6125c8b4fa82e36a457073
brunsilva-jc <brunsilva.jc@gmail.com>
2024-09-06 09:22:31 -0300

    created new class model

---
+++ people.py
@@ -0,0 +1,6 @@
+class People:
+    def __init__(self, name, gender, height, birth_year):
+        self.name = name
+        self.gender = gender
+        self.height = height
+        self.birth_year = birth_year
